997c3137aa48650cef45a91b571550868046deb6
Harman Mehra <harman4mehra@gmail.com>
2026-03-31 15:42:40 +0530

    feat: added error tracking using Sentry

--- server/controllers/clerk.ts
+++ server/controllers/clerk.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { verifyWebhook } from "@clerk/express/webhooks";
-import { log } from 'node:console';
 import { prisma } from '../configs/prisma.js'
+import * as Sentry from "@sentry/node"
 
 const clerkWebhooks = async (req: Request, res: Response) => {
     try {
@@ -71,6 +71,7 @@ const clerkWebhooks = async (req: Request, res: Response) => {
         res.json({message: "Webhook Recieved : " + type})
 
     } catch (error: any) {
+        Sentry.captureException(error)
         res.status(500).json({ message: error.message });
     }
 }
